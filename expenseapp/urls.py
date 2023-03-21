d6de66e8506163b40254699376c850cb418d98ca
rohit bisht <rohit.bisht@disecto.com>
2023-03-21 17:03:46 +0530

    First commit

---
+++ expenseapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import index, show_expenses,edit,delete
+urlpatterns = [
+    path('',index,name='index'),
+    path('expense/',show_expenses,name='expense'),
+    path('edit/<int:id>/',edit,name='edit'),
+    path('delete/<int:id>/',delete,name='delete'),
+] 
