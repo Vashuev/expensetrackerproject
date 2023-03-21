3bc6e977ce180d0a7861d092a92e51084910e5d3
rohit bisht <rohit.bisht@disecto.com>
2023-03-21 18:06:35 +0530

    media in url

--- expensetrackerproject/asgi.py
+++ expensetrackerproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for expenseproject project.
+ASGI config for expensetrackerproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expenseproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expensetrackerproject.settings')
 
 application = get_asgi_application()
